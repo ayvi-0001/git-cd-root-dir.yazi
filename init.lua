78860da194f7684872278a0336d3f6d76cdea4a9
ayvi-0001 <alankvickers+github@gmail.com>
2025-05-28 14:43:17 -0700

    feat: update for yazi v25.5.28
    add [`@since`](https://yazi-rs.github.io/docs/plugins/overview#@since) annotation
    remove local definition file: types.lua
    add .luarc.json, use lib yazi-rs/plugins:types
    replace deprecated Command fn `args()` for `arg()`
    replace deprecated ya fn `mgr_emit()` for `emit()`
    refactor url helper functions

--- init.lua
+++ init.lua
@@ -1,15 +1,6 @@
----@diagnostic disable: undefined-global
-
----@module 'ya', 'fs'
----@diagnostic disable-next-line: unknown-cast-variable
----@cast ya Ya
----@diagnostic disable-next-line: unknown-cast-variable
----@cast fs Fs
+--- @since 25.5.28
 
 local M = {}
-
----@type number
-TIMEOUT_NOTIFY = 2.0
 
 ---Check if var is none. Converted to string first.
 ---@param v any
@@ -19,16 +10,31 @@ local isempty = function(v)
   return s == nil or s == ""
 end
 
----@param url1 (Url|string)?
----@param url2 (Url|string)?
-local _url_eq = function(url1, url2)
+---@param url1 Url|string?
+---@param url2 Url|string?
+local url_eq = function(url1, url2)
   if type(url1) == "string" then
-    url1 = Url(url1) --[[@as Url]]
+    url1 = Url(url1) ---@cast url1 Url
   end
   if type(url2) == "string" then
-    url2 = Url(url2) --[[@as Url]]
+    url2 = Url(url2) ---@cast url2 Url
   end
   return select(1, fs.unique_name(url2)) == select(1, fs.unique_name(url1))
+end
+
+---Search parent paths of a Url for a suffix.
+---@param path Url
+---@param suffix string
+---@return Url?
+local function url_recursive_search(path, suffix)
+  local parent = path.parent
+  if path:ends_with(suffix) then
+    return parent
+  elseif parent then
+    return url_recursive_search(parent, suffix)
+  else
+    return nil
+  end
 end
 
 ---@enum LogLevel
@@ -45,36 +51,37 @@ local ya_notify = function(content, level)
     title = "git-cd-root-dir",
     content = content,
     level = level or LOGLEVEL.info,
-    timeout = TIMEOUT_NOTIFY,
+    timeout = 2.0,
   }
 end
 
 ---@async
----@param job { args: string[]? }
+---@param job { args: { [integer|string]: Sendable? } }
 function M:entry(job) ---@diagnostic disable-line: unused-local
   local cwd = fs.cwd()
 
-  local cmd = Command "git" --[[@as Command]]
-  local output = cmd:args({ "rev-parse", "--show-toplevel" }):cwd(tostring(cwd)):output()
+  local git = Command "git"
+  local output, err = git:arg({ "rev-parse", "--show-toplevel" }):cwd(tostring(cwd)):output()
 
   if not output then
+    ya_notify("Error: " .. err, LOGLEVEL.error)
     return
   end
 
   local result = output.stdout:match("^%s*(.-)%s*$"):gsub("[\n\r]+", " ") --[[@as string]]
 
   -- Check if we are already in the root dir.
-  if _url_eq(cwd, result) then
+  if url_eq(cwd, result) then
     ya_notify("Already in the root dir of this repo.", LOGLEVEL.info)
     return
   end
 
   -- Try to change dirs.
-  ya.mgr_emit("cd", { result })
+  ya.emit("cd", { result })
 
   local latest_cwd = fs.cwd()
 
-  if not _url_eq(cwd, latest_cwd) then
+  if not url_eq(cwd, latest_cwd) then
     -- We've changed directories, return.
     return
   end
@@ -83,25 +90,10 @@ function M:entry(job) ---@diagnostic disable-line: unused-local
   -- check to see if we are in a .git dir, since the result of
   -- rev-parse --show-toplevel will error if its not ran in a work tree.
 
-  ---Search parent paths of a Url for a suffix.
-  ---@param path Url
-  ---@param suffix string
-  ---@return Url?
-  local function recurse_dir_search(path, suffix)
-    local parent = path.parent
-    if path:ends_with(suffix) then
-      return parent
-    elseif parent then
-      return recurse_dir_search(parent, suffix)
-    else
-      return nil
-    end
-  end
-
-  local ok, pcall_result = pcall(recurse_dir_search, latest_cwd, ".git")
+  local ok, pcall_result = pcall(url_recursive_search, latest_cwd, ".git")
 
   if ok and pcall_result ~= nil then
-    ya.mgr_emit("cd", { pcall_result })
+    ya.emit("cd", { pcall_result })
   elseif isempty(result) then
     -- Finally, confirm the initial result is empty, we are not in a git repository.
     ya_notify("Not in a git repository.", LOGLEVEL.error)
